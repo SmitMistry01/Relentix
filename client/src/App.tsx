b3abe3c4b1391ed87bb1b93af02af31961434b42
SmitMistry01 <smitmistry36466@gmail.com>
2025-06-14 16:44:08 +0530

    added:navbar,sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,22 @@
 import './App.css'
 import DashboardWrapper from './app/DashboardWrapper'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Settings from './app/components/Settings';
 
 function App() {
-
   return (
-    <>
-      <DashboardWrapper>
-        <h3 className='text-xl font-bold'>
-          This is the Dashboard
-        </h3>
-      </DashboardWrapper>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <DashboardWrapper>
+            <h3 className='text-xl font-bold'>
+              This is the Dashboard
+            </h3>
+          </DashboardWrapper>
+        } />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- client/src/app/components/Sidebar.tsx
+++ client/src/app/components/Sidebar.tsx
@@ -1,13 +1,44 @@
+'use client'
+
+import { useState } from "react";
+import { LockIcon } from "lucide-react";
+
 const Sidebar = () => {
-    return(
-        <aside className="h-screen">
-            <nav className="h-full flex flex-col bg-white border-r shadow-sm">
-                <div className="p-4 pb-2 flex justify-between items-center">
-                    <img src="./src/assets/logo.png" className="w-8 h-8" alt="logo"/>
+    const [showProjects, setShowProjects] = useState(true);
+    const [showPriority, setShowPriority] = useState(true);
+
+    const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white w-64`;
+
+    return (
+        <div className={sidebarClassNames}>
+            <div className="flex h-[100%] w-full flex-col justify-start">
+                {/* TOP LOGO */}
+                <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
+                    <div className="text-xl font-bold text-gray-800 dark:text-white">
+                        EDLIST
+                    </div>
                 </div>
-            </nav>
-        </aside>
-    )
+            
+            <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+                <img
+                    src="./src/assets/logo.png"
+                    alt="Logo"
+                    className="w-8 h-8"
+                />
+                <div>
+                    <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+                        EDROH TEAM
+                    </h3>
+                    <div className="mt-1 flex items-start gap-2">
+                        <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
+                        <p className="text-xs text-gray-500">Private</p>
+                    </div>
+                </div>
+            </div>
+            {/* Navbar Links */}
+        </div>
+        </div>
+    );
 }
 
 export default Sidebar;
