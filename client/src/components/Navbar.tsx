f55c0e01870a275db4e29f7b3c098705976a6ee3
SmitMistry01 <smitmistry36466@gmail.com>
2025-06-27 23:36:52 +0530

    d

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
   return (
-    <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
+    <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black"> 
       {/* Search Bar */}
       <div className="flex items-center gap-8">
         {!isSidebarCollapsed ? null : (
